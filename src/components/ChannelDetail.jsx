45cb6c499c53c61d7556219af2ae7c7af077c062
Shimin <natkuma@outloook.com>
2023-08-03 11:55:18 -0400

    missing hover effect

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, useLayoutEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams(); 
+  console.log(videos)
+
+  useEffect(() => {
+    // const fetchResults = async () => {
+    //   const data = await fetchFromAPI(`channels?part=snippet&id=${id}`)}})
+    //   setChannelDetail(data.items[0])
+    fetchFromAPI(`channels?part="snippet&id=${id}`)
+      .then((data) => setChannelDetail(data.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos (data.items));
+    }, [id])
+
+  
+  return (
+    <Box>
+      <Box>
+        <ChannelCard channelDetail={channelDetail} />
+      </Box>
+      <Box display= "flex" p="2">
+        <Box sx={{ mr: { sm: '100px' }}} />
+          <Videos videos={videos} />
+      </Box>
+    </Box>
+  )
+}
+
+export default ChannelDetail;
---
+++ src/components/Feed.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect( () => {
+     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "coloumn", md: "row" } }}>
+      <Box 
+      sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+      <Sidebar 
+      selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}
+      />
+      <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: 'white' }}>
+      Copyright @ 2023 NAT Media
+      </Typography>
+      </Box>
+
+{/* the title of the videos category */}
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          {selectedCategory} <span style={{ color: '#ff48a5' }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+      
+    </Stack>
+  )
+}
+
+export default Feed
