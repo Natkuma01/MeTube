192671f27c58049d4098d4702fa46604f4ee0521
Shimin <natkuma@outloook.com>
2023-09-30 11:43:19 +0800

    rerrange all paths inside Routes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import { Box } from '@mui/material';
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter><Routes>
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar /> 
       <Route path='/' element={<Feed />} />
-      <Routes>
-        <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:searchTerm' element={<SearchFeed />} />
-      </Routes>
-    </Box>
+      <Route path='/video/:id' element={<VideoDetail />} />
+      <Route path='/channel/:id' element={<ChannelDetail />} />
+      <Route path='/search/:searchTerm' element={<SearchFeed />} />
+    </Box> </Routes>
   </BrowserRouter>
 );
 
