2da85883124a059f875a9f8fbc1243a1bf3c723f
Shimin <natkuma@outloook.com>
2023-09-30 12:05:35 +0800

    change routes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Box } from '@mui/material';
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
-  <BrowserRouter><Routes>
+  <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
+      <Routes>
       <Navbar /> 
-      <Route path='/' element={<Feed />} />
+      <Route path='/' element={<Feed />} /> 
+      </Routes>
       <Route path='/video/:id' element={<VideoDetail />} />
       <Route path='/channel/:id' element={<ChannelDetail />} />
       <Route path='/search/:searchTerm' element={<SearchFeed />} />
-    </Box> </Routes>
+    </Box> 
   </BrowserRouter>
 );
 
