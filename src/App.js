a3c3d0bdae69595431beda805a253fbdadbcfd59
Shimin <natkuma@outloook.com>
2023-10-03 10:42:16 +0800

    change BrowserRouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { BrowserRouter, HashRouter, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />   
-      <HashRouter>
+      <Routes>
       <Route path='/' element={<Feed />} /> 
       <Route path='/video/:id' element={<VideoDetail />} />
       <Route path='/channel/:id' element={<ChannelDetail />} />
       <Route path='/search/:searchTerm' element={<SearchFeed />} />
-     </HashRouter>
+     </Routes>
      </Box> 
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
